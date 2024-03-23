da965bd8c9bc80e2a0c8c8e18619936967d87c14
Emir <emirtuncer35@hotmail.com>
2024-03-24 00:11:30 +0300

    App renamed

--- ios/YanOdam/AppDelegate.mm
+++ ios/YanOdam/AppDelegate.mm
@@ -7,12 +7,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"YanOdamRN";
+  self.moduleName = @"YanOdam";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
   [FIRApp configure];
-  
+
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
--- ios/YanOdamTests/YanOdamTests.m
+++ ios/YanOdamTests/YanOdamTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface YanOdamRNTests : XCTestCase
+@interface YanOdamTests : XCTestCase
 
 @end
 
-@implementation YanOdamRNTests
+@implementation YanOdamTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
